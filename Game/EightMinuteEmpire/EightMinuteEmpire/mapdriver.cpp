e32a1cc0b595cb408f17689d9768b0a66aea6fad
Martin <Martin@DESKTOP-MARTIN>
2019-10-08 17:03:05 -0400

    works v4 - tested with final maps !

--- Game/EightMinuteEmpire/EightMinuteEmpire/mapdriver.cpp
+++ Game/EightMinuteEmpire/EightMinuteEmpire/mapdriver.cpp
@@ -12,12 +12,20 @@ int main()
 
 	Board& mapofworld = Board::getInstance();	
 
-	mapofworld.readfile("C:\\map8.txt"); // SUCCESS
+	//mapofworld.readfile("C:\\map1.txt"); // SUCCESS
+	//mapofworld.readfile("C:\\map5.txt"); // SUCCESS
+	//mapofworld.readfile("C:\\map6.txt"); // SUCCESS
+	//mapofworld.readfile("C:\\map8.txt"); // SUCCESS
+	//mapofworld.readfile("C:\\map9.txt"); // SUCCESS
+	//mapofworld.readfile("C:\\map10.txt"); // SUCCESS
+	//mapofworld.readfile("C:\\map12.txt"); // SUCCESS
+	//mapofworld.readfile("C:\\map14.txt"); // SUCCESS
+
 	//mapofworld.readfile("C:\\fail8.txt"); // DISCONNECTED GRAPH
 
 	mapofworld.printlist();
 
-	mapofworld.DFS("R06");
+	mapofworld.DFS("R02");
 
 	return 0;
 }
