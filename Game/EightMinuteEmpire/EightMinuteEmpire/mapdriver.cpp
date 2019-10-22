afa844cbc3e9145e8580976d53e04f2cfa8bfeb8
Martin <Martin@DESKTOP-MARTIN>
2019-10-22 14:25:30 -0400

    removed old test lines from mapdriver

--- Game/EightMinuteEmpire/EightMinuteEmpire/mapdriver.cpp
+++ Game/EightMinuteEmpire/EightMinuteEmpire/mapdriver.cpp
@@ -13,8 +13,6 @@ static int mapDriver()
 
 	Board& mapofworld = Board::getInstance();	
 		
-	//mapofworld.readfile("resource/map6.txt"); 
-	//mapofworld.readfile("resource/failformat.txt");
 	mapofworld.loadmap();
 
 	return 0;
