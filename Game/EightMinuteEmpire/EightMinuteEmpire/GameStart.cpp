12a2094a6e4f1570b5d4c451d8080440f01e0704
Martin <martypolish@gmail.com>
2019-10-26 17:11:46 -0400

    wrote code the way it should be, beginning investigation

--- Game/EightMinuteEmpire/EightMinuteEmpire/GameStart.cpp
+++ Game/EightMinuteEmpire/EightMinuteEmpire/GameStart.cpp
@@ -1,5 +1,4 @@
 #include "map.h"
-#include "Player.h"
 #include "GameStart.h"
 #include <iostream>
 #include <fstream>
@@ -9,10 +8,43 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+GameStart::GameStart() {
 
+	Board& mapofworld = Board::getInstance();
+	mapofworld.loadmap();
+	gamedeck = loadDeck(loadPlayers());
 
+}
 
-static int GameLoader()
+int GameStart::loadPlayers() {
+	string playerName;
+	int nbPlayers, playerAge, count = 1;
+	cout << "\n How many players will be playing?" << endl;
+	cin >> nbPlayers;
+
+	for (int i = 0; i < nbPlayers; i++) {
+
+		cout << "Player " << count << " what is your name?" << endl;
+		cin >> playerName;
+		cout << "Player " << count << " what is your age?" << endl;
+		cin >> playerAge;
+
+		Player* tPlayer = new Player(playerName, playerAge);
+		count++;
+
+	}
+	return nbPlayers;
+	
+}
+
+Deck* GameStart::loadDeck(int nbPlayers) {
+
+	Deck* gamedeck = new Deck(nbPlayers);
+
+	return gamedeck;
+}
+
+int GameLoader()
 {
 
 	GameStart& gameplay = GameStart::getInstance();
--- Game/EightMinuteEmpire/EightMinuteEmpire/GameStart.h
+++ Game/EightMinuteEmpire/EightMinuteEmpire/GameStart.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <string>
 #include "Cards.h"
+#include "Player.h"
 
 using std::cout;
 using std::endl;
@@ -28,40 +29,10 @@ public:
 
 private:
 
-	GameStart() {
+	GameStart();
 
-		Board& mapofworld = Board::getInstance();
-		 mapofworld.loadmap();
-		 gamedeck = loadDeck(loadPlayers());
-		
-	};
-
-	int loadPlayers() {
-		string playerName;
-		int nbPlayers, playerAge, count = 1;
-		cout << "\n How many players will be playing?" << endl;
-		cin >> nbPlayers;
-
-		for (int i = 0; i < nbPlayers; i++) {
-
-			cout << "Player " << count << " what is your name?" << endl;
-			cin >> playerName;
-			cout << "Player " << count << " what is your age?" << endl;
-			cin >> playerAge;
-
-			Player* tPlayer = new Player(playerName, playerAge);
-			count++;
-
-		}
-		return nbPlayers;
-		//GameStart::loadDeck(nbPlayers);
-	}
+	int loadPlayers();
 	
-	inline Deck* loadDeck(int nbPlayers) {
-
-		Deck* gamedeck = new Deck(nbPlayers);
-
-		return gamedeck;
-	}
+	Deck* loadDeck(int nbPlayers);
 	
 };
