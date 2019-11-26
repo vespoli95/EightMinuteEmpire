abff92a91a77b84d6f4779241eb157ff93fa06b2
Martin <Martin@DESKTOP-MARTIN>
2019-11-26 09:32:22 -0500

    added invalid input handling

--- Game/EightMinuteEmpire/EightMinuteEmpire/cardsfactorydriver.cpp
+++ Game/EightMinuteEmpire/EightMinuteEmpire/cardsfactorydriver.cpp
@@ -7,11 +7,17 @@ using std::endl;
 
 static int cardsfactorydriver(){
 
-
+	bool keepgoing = true;
 	int nbPlayers = 0;
+	
+	cout << "How many players will be playing?" << endl;
 
-	cout << "How many players will be playing? (enter number from 1 to 5)" << endl;
-	cin >> nbPlayers;
+	while ((std::cout << "Enter a number from 1 to 5:")	&& (!(std::cin >> nbPlayers) || nbPlayers < 1 || nbPlayers > 5)) {
+		std::cout << "That's not a number between 1 and 5; ";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+
 	cout << "Setting up deck" << endl;
 	Deck* deck = new Deck(nbPlayers);
 	deck->toString();
